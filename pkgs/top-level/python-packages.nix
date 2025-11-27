d5c248de81dfa5fa5b861a178ca3738f57fa66f8
Robert Schütz <nix@dotlambda.de>
2025-11-26 16:12:52 -0800

    python3Packages.energyid-webhooks: init at 0.0.14

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4843,6 +4843,8 @@ self: super: with self; {
 
   energyflow = callPackage ../development/python-modules/energyflow { };
 
+  energyid-webhooks = callPackage ../development/python-modules/energyid-webhooks { };
+
   energyzero = callPackage ../development/python-modules/energyzero { };
 
   enlighten = callPackage ../development/python-modules/enlighten { };
